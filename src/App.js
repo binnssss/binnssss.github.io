7ff6f71520450dcaca2c17cbcbac1b2f6fea2f05
binnssss <40708149+binnssss@users.noreply.github.com>
2022-02-04 10:03:39 +0800

    set homepage to under development

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PAGE TO BE RENOVATED</h1>
+      
+      <img src="https://www.freepnglogos.com/uploads/wrench/wrench-in-hands-logo-icon-25.png" alt="wrench-icon"/>
+      <p>
+
+        Thank you for your interest,<br/>
+        Will be back with webpage changes.
+
+      </p>
+
+      <p>Checkout My Friends Portfolios instead: <br/>
+      
+      <a href='https://www.instagram.com/pellamuti/'>Pellamuti</a>
+
+      </p>
+
+
+    </div>
+  );
+}
+
+export default App;
